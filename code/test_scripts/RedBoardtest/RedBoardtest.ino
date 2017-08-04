94bcfdb366e3c6eca97699436b2cf60e270d2d95
Odeh Saba <odehsaba@Odehs-MacBook-Pro.local>
2017-08-03 21:39:42 -0700

    Removed some unecessary comments.  Cleaned up code

--- code/test_scripts/RedBoardtest/RedBoardtest.ino
+++ code/test_scripts/RedBoardtest/RedBoardtest.ino
@@ -1,37 +1,11 @@
-//int pin1 = 8;
-//int pin2 = 0;
-//
-//void setup(){
-//  pinMode(pin1, OUTPUT);
-//  pinMode(pin2, INPUT);
-//
-//  digitalWrite(pin1,LOW);
-//
-//  Serial.begin(9600);
-//}
-//
-//void loop(){
-//  digitalWrite(pin1, HIGH);
-//  Serial.print("Output Pin = ");
-//  Serial.print(pin1);
-//  Serial.print("\n A0 =");
-//  Serial.print(analogRead(pin2));
-//  Serial.print("\n");
-//  delay(3000);
-//  digitalWrite(pin1, LOW);
-//  Serial.print("Output Pin = ");
-//  Serial.print(pin1);
-//  Serial.print("\n A0 =");
-//  Serial.print(analogRead(pin2));
-//  Serial.print("\n");
-//  delay(3000);
-//}
-
 //Mux control pins
-//#define s0 3
-//#define s1 4
-//#define s2 5
-//#define s3 8
+// ARDUINO PINOUT
+//#define s0 8
+//#define s1 9
+//#define s2 10
+//#define s3 11
+
+// RED BOARD PINOUT
 #define s0 2
 #define s1 6
 #define s2 7
@@ -97,10 +71,6 @@ void test3() {
     Serial.println(analogRead(SIGpin));
   }
 
-  
-
-  
-  
 }
 
 
@@ -145,24 +115,24 @@ void loop(){
 int readMux(int channel){
 //  int controlPin[] = {s0, s1, s2, s3};
 
-  int muxChannel[16][4]={
-    {0,0,0,0}, //channel 0
-    {1,0,0,0}, //channel 1
-    {0,1,0,0}, //channel 2
-    {1,1,0,0}, //channel 3
-    {0,0,1,0}, //channel 4
-    {1,0,1,0}, //channel 5
-    {0,1,1,0}, //channel 6
-    {1,1,1,0}, //channel 7
-    {0,0,0,1}, //channel 8
-    {1,0,0,1}, //channel 9
-    {0,1,0,1}, //channel 10
-    {1,1,0,1}, //channel 11
-    {0,0,1,1}, //channel 12
-    {1,0,1,1}, //channel 13
-    {0,1,1,1}, //channel 14
-    {1,1,1,1}  //channel 15
-  };
+//  int muxChannel[16][4]={
+//    {0,0,0,0}, //channel 0
+//    {1,0,0,0}, //channel 1
+//    {0,1,0,0}, //channel 2
+//    {1,1,0,0}, //channel 3
+//    {0,0,1,0}, //channel 4
+//    {1,0,1,0}, //channel 5
+//    {0,1,1,0}, //channel 6
+//    {1,1,1,0}, //channel 7
+//    {0,0,0,1}, //channel 8
+//    {1,0,0,1}, //channel 9
+//    {0,1,0,1}, //channel 10
+//    {1,1,0,1}, //channel 11
+//    {0,0,1,1}, //channel 12
+//    {1,0,1,1}, //channel 13
+//    {0,1,1,1}, //channel 14
+//    {1,1,1,1}  //channel 15
+//  };
 
   //loop through the 4 sig
   for(int i = 0; i < 4; i ++){
