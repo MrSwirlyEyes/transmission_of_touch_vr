d90450405952046d3d1f59a5217727f6146a9b5d
Odeh Saba <odehsaba@Odehs-MacBook-Pro.local>
2017-08-09 20:26:33 -0700

    Updated RBRecieve and RBtransmit code

--- code/test_scripts/RBRecieve/RBRecieve.ino
+++ code/test_scripts/RBRecieve/RBRecieve.ino
@@ -1,15 +1,30 @@
 #include <radio.h>
+#include <Wire.h>
+#include <Adafruit_PWMServoDriver.h> //library needs to be downloaded
 
 #define CHANNEL 11
+#define SERVOMIN  150 //This is the 'minimum' pulse length count (out of 4096).
+#define SERVOMAX  600 //This is the 'maximum' pulse length count (out of 4096).
+
+Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver();
 
 Radio radio(CHANNEL);  // Initialize ATmega128RFA1 radio on channel CHANNEL (can be 11-26)
 
+int servoNum, angle, i;
+
 struct Packet {
-  int test1= 0;
 
-  int test2= 0;
+  int start_check = 0;
 
-  int test3= 0;
+  int sensordat[16];
+  
+//  int test1= 0;
+//
+//  int test2= 0;
+//
+//  int test3= 0;
+
+  int end_check = 0;
   
 };
 
@@ -21,6 +36,11 @@ Packet pkt;
 void setup()
 {
   Serial.begin(9600);
+
+   //Set up PWM.
+  pwm.begin();
+  pwm.setPWMFreq(60);
+
   
 }
 
@@ -28,12 +48,54 @@ void loop()
 {
   if(radio.rfAvailable()) {           // If data receievd on radio...
     radio.rfRead((uint8_t *) & pkt,sizeof(Packet)); // ... send it out serial.
-    Serial.print("Test1: ");
-    Serial.println(pkt.test1);
-    Serial.print("Test2: ");
-    Serial.println(pkt.test2);
-    Serial.print("Test3: ");
-    Serial.println(pkt.test3);
+
+    if(valid_pkt_check(pkt)) {
+      //Loop through and read all 16 values
+      //Reports back Value at channel 6 is: 346
+      for(int j = 0; j < 5 ; j++) {
+        Serial.print("Raw Flex vall");
+        Serial.print(j);
+        Serial.print(": ");
+        Serial.println(pkt.sensordat[j]);
+        angle = map(pkt.sensordat[j],0,1000,0,180);
+        turnTo(j, angle);
+      }
+      for(int j = 5; j<10; j++){
+        Serial.print("Raw Pressure vall");
+        Serial.print(j-5);
+        Serial.print(": ");
+        Serial.println(pkt.sensordat[j]);
+        angle = map(pkt.sensordat[j],800,200,0,180);
+        turnTo(j, angle);
+      }
+    
+//      Serial.print("Test1: ");
+//      Serial.println(pkt.test1);
+//      Serial.print("Test2: ");
+//      Serial.println(pkt.test2);
+//      Serial.print("Test3: ");
+//      Serial.println(pkt.test3);
+    }
+    else { Serial.print("Error");
+    }
   }
-  
 }
+
+bool valid_pkt_check(Packet & pkt) {
+  if(pkt.start_check =! 0xFEEAED){
+    return false;
+  }
+  else if(pkt.end_check =! 0xDAAABD){
+    return false;
+  }
+  return true;
+}
+
+void turnTo(int servoNum, int angle)
+{
+  angle = map(angle, 0, 180*4, SERVOMIN, SERVOMAX);
+  pwm.setPWM(servoNum, 0, angle);
+}
+
+
+  
--- code/test_scripts/RBTransmit/RBTransmit.ino
+++ code/test_scripts/RBTransmit/RBTransmit.ino
@@ -1,29 +1,70 @@
-#include <radio.h>
+//#include <radio.h>
 
-#define CHANNEL 11
+#define s0 2 
+#define s1 6 
+#define s2 7 
+#define s3 10
+#define SIGpin 0
 
-Radio radio(CHANNEL);  // Initialize ATmega128RFA1 radio on channel CHANNEL (can be 11-26)
+//Radio radio(11);  // Initialize ATmega128RFA1 radio on channel CHANNEL (can be 11-26)
 
 struct Packet {
-  int test1= 0;
+  
+  int start_check = 0xFEEAED;
 
-  int test2= 0;
+  int sensordat[16];
 
-  int test3= 0;
+  int end_check = 0xDAAABD;
   
 };
 
 Packet pkt;
 
+byte controlPin[]={s0,s1,s2,s3};
+
+bool muxChannel[16][4]={
+    {0,0,0,0}, //channel 0
+    {1,0,0,0}, //channel 1
+    {0,1,0,0}, //channel 2
+    {1,1,0,0}, //channel 3
+    {0,0,1,0}, //channel 4
+    {1,0,1,0}, //channel 5
+    {0,1,1,0}, //channel 6
+    {1,1,1,0}, //channel 7
+    {0,0,0,1}, //channel 8
+    {1,0,0,1}, //channel 9
+    {0,1,0,1}, //channel 10
+    {1,1,0,1}, //channel 11
+    {0,0,1,1}, //channel 12
+    {1,0,1,1}, //channel 13
+    {0,1,1,1}, //channel 14
+    {1,1,1,1}  //channel 15
+  };
+
 void setup() {
   // put your setup code here, to run once:
-  pkt.test1 = 5;
-  pkt.test2 = 6;
-  pkt.test3 = 7;
+  pinMode(s0,OUTPUT);
+  pinMode(s1,OUTPUT);
+  pinMode(s2,OUTPUT);
+  pinMode(s3,OUTPUT);
+
+  digitalWrite(s0, LOW);
+  digitalWrite(s1, LOW);
+  digitalWrite(s2, LOW);
+  digitalWrite(s3, LOW);
+
+  Serial.begin(9600);
+
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  
-  radio.rfWrite((uint8_t *) & pkt, sizeof(Packet));
+  for(int j = 0; j<10; j++) {
+    for(int i = 0; i<4; i++){
+      digitalWrite(controlPin[i], muxChannel[j][i]);
+    }
+    Serial.println(analogRead(SIGpin));
+    
+    pkt.sensordat[j] = analogRead(SIGpin);
+  }
+  //radio.rfWrite((uint8_t *) & pkt, sizeof(Packet));
 }
