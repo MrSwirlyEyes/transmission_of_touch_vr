240e2ba858f1657e7bb94954833430944be0ae39
Stazia Tronboll <WigginWannabe@users.noreply.github.com>
2018-02-25 00:45:27 -0800

    model fingers bend in response to flex sensors. I can't map them properly, though...so the hand is doing some creepy ass shit.

--- Assets/Scripts/Communicator.cs
+++ Assets/Scripts/Communicator.cs
@@ -12,8 +12,8 @@ public class Communicator : MonoBehaviour {
 
 	private SerialPort stream;
 
-	public int minFlex = 20;
-	public int maxFlex = 110;
+	public int minFlex = 60;
+	public int maxFlex = 240;
 	public int minFinger = 10;
 	public int maxFinger = 90;
 	public int maxThumb = 50;
@@ -57,7 +57,7 @@ public class Communicator : MonoBehaviour {
 			try {
 				dataString = stream.ReadLine ();
 //				stream.BaseStream.Flush();
-			} catch (TimeoutException e) {
+			} catch (TimeoutException) {
 //				Debug.LogError(e);
 				dataString = null;
 			}
@@ -104,18 +104,20 @@ public class Communicator : MonoBehaviour {
 
 		char[] delimiters = { ',' };
 		string[] values = inData.Split (delimiters);
-		Debug.Log("Receiving " + inData);
+//		Debug.Log("Receiving " + inData);
 
-		knuckles.thumb = map(System.Convert.ToInt32 (values [0]),maxFlex,minFlex,minFinger,maxThumb);
+		knuckles.thumb = map(System.Convert.ToInt32 (values [0]),minFlex,maxFlex,minFinger,maxThumb);
 		knuckles.index = map(System.Convert.ToInt32 (values [1]),maxFlex,minFlex,minFinger,maxFinger);
 		knuckles.middle =  map(System.Convert.ToInt32 (values [2]),maxFlex,minFlex,minFinger,maxFinger);
 		knuckles.ring =  map(System.Convert.ToInt32 (values [3]),maxFlex,minFlex,minFinger,maxFinger);
 		knuckles.pinky = map(System.Convert.ToInt32 (values [4]),maxFlex,minFlex,minFinger,maxFinger);
-
+		Debug.Log ("Thumb: " + knuckles.thumb);
 	}
 
 	int map(int x, int in_min, int in_max, int out_min, int out_max) {
-		return (x-in_min)*(out_max-out_min)/(in_max-in_min) + out_min;
+		Debug.Log (x);
+		Debug.Log (((x - in_min) * ((out_max - out_min) / (in_max - in_min))) + out_min);
+		return (int) (out_max - (x - in_min)*1f*(out_max - out_min)/(in_max - in_min) + out_min);
 	}
 
 	void writeDefault() {
